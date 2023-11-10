af1c920868a8a86531b85ed79324c47547fbc341
odd <flinqwe@gmail.com>
2023-11-10 15:37:50 +0200

    Add project files.

---
+++ TicTacToeApi/Startup.cs
@@ -0,0 +1,52 @@
+﻿namespace TicTacToeApi
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            this.Configuration = Configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Database
+
+
+            // Identity configuring
+
+            
+            // JWT configuring
+
+            
+            // AutoMapper
+
+
+            // DI
+        }
+
+        public void Configuring(IApplicationBuilder app, IWebHostEnvironment environment) 
+        {
+            if (environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
